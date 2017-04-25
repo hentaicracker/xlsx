e863b54d94d6bac167461b4997b518c881ef0ea2
hentaicracker <641232582@qq.com>
2017-04-25 16:20:28 +0800

    read xlsx file

--- read.js
+++ read.js
@@ -2,8 +2,23 @@ var XLSX = require('xlsx');
 var workbook = XLSX.readFile('test.xlsx');
 var sheetNames = workbook.SheetNames;
 var workSheet = workbook.Sheets[sheetNames[0]];
+var data = XLSX.utils.sheet_to_json(workSheet);
 
-for(var z in workSheet) {
-    if(z[0] === '!') continue;
-    console.log(z + '=' + JSON.stringify(workSheet[z].v));
-}
+// var headerMap = {
+//     terminalName: '终端花名（必填）',
+//     deviceModelName: '机型名称（必填）',
+//     productionDate: '出厂日期（必填）',
+//     isMasterTerminal: '是否为主柜（必填）',
+//     masterTerminalName: '主柜终端花名'
+// };
+
+// let arr = data.map(i => Object.assign({}, {
+//     terminalName: i[headerMap['terminalName']],
+//     deviceModelName: i[headerMap['deviceModelName']],
+//     productionDate: i[headerMap['productionDate']],
+//     isMasterTerminal: i[headerMap['isMasterTerminal']],
+//     masterTerminalName: i[headerMap['masterTerminalName']] || null,
+// }));
+
+console.log(sheetNames);
+console.log(workSheet['!merges']);
